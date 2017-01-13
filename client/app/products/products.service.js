5046a19c0dd951b9a7d2b700773a46dd9b54396c
Jeff Hong Xiao <jeff.hong.xiao@gmail.com>
2017-01-13 19:45:18 -0400

    add methods to be returned in Products service

--- client/app/products/products.service.js
+++ client/app/products/products.service.js
@@ -2,11 +2,53 @@
 
 angular.module('meanshopApp')
   .factory('Products', function () {
-    // Service logic
-    // ...
+    var last_id = 2;
 
-    // Public API here
-    return [
+    var example_products = [
       {_id: 1, title: 'Product 1', price: 123.45, quantity: 10, description: 'Description of Product 1'},
       {_id: 2, title: 'Product 2', price: 123.45, quantity: 5, description: 'Description of Product 2'}];
+
+    // Public API here
+    return {
+      query: function() {
+        return example_products;
+      },
+
+      get: function(param) {
+        var result = {};
+        angular.forEach(example_products, function(product) {
+          if (product._id == params.id) {
+            return this.product = product;
+          }},
+        result);
+
+        return result.product;
+      },
+
+      delete: function(params) {
+        angular.forEach(example_products, function(product, index) {
+          if (product._id == params._id) {
+            console.log(product, index);
+            example_products.splice(index, 1);
+            return;
+          }
+        });
+      },
+
+      create: function(product) {
+        product.id = ++last_id;
+        example_products.push(product);
+      },
+
+      update: function(product) {
+        var item = this.get(product);
+        if (!item) return false;
+
+        item.title = product.title;
+        item.price = product.price;
+        item.quantity = product.quantity;
+        item.description = product.description;
+        return true;
+      }
+    };
   });
