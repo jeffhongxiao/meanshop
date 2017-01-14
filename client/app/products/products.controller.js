851502c38fa9d5e712d911b413cfc0264be0c519
Jeff Hong Xiao <jeff.hong.xiao@gmail.com>
2017-01-14 06:57:48 -0400

    add CRUD to products.controller.js

--- client/app/products/products.controller.js
+++ client/app/products/products.controller.js
@@ -2,5 +2,29 @@
 
 angular.module('meanshopApp')
   .controller('ProductsCtrl', function ($scope, Products) {
-    $scope.products = Products;
-  });
+    $scope.products = Products.query();
+  })
+  .controller('ProductViewCtrl', function($scope, $state, $stateParams, Product) {
+    $scope.product = Products.get({id: $stateParams.id});
+
+    $scope.deleteProduct = function() {
+      Products.delete($scope.product);
+      $state.go('products');
+    };
+  })
+  .controller('ProductNewCtrl', function($scope, $state, Product) {
+    $scope.product = {};
+
+    $scope.addProduct = function(product) {
+      Products.create($scope.product);
+      $state.go('products');
+    };
+  })
+  .controller('ProductEditCtrl', function($scope, $state, $stateParams, Product) {
+    $scope.product = Products.get({id: $stateParams.id});
+    $scope.editProduct = function(product) {
+      Products.update($scope.product);
+      $state.go('products');
+    }
+  })
+  ;
