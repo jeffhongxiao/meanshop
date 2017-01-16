b3af937e2357c7ccf81e21b7fcd876e4150df9d0
Jeff Hong Xiao <jeff.hong.xiao@gmail.com>
2017-01-15 21:21:09 -0400

    chapter 5 initial commit

--- client/app/products/products.service.js
+++ client/app/products/products.service.js
@@ -1,64 +1,8 @@
 'use strict';
 
 angular.module('meanshopApp')
-  .factory('Products', function () {
-    var last_id = 3;
-
-    var example_products = [
-      {_id: 1, title: 'Inspiron 5720', price: 1234.5, quantity: 10, description: 'Inspiron 5720 is a good laptop.'},
-      {_id: 2, title: 'MacBook Pro 2016', price: 1999.99, quantity: 5, description: 'MacBook Pro laptops are fancy but expensive.'},
-      {_id: 3, title: 'HP Stream Cloudbook', price: 350, quantity: 10, description: 'Cloudbook and Chromebook laptops are like netbooks.'}
-    ];
-
-    // Public API here
-    return {
-      query: function() {
-        return example_products;
-      },
-
-      get: function(params) {
-        /*** use angular.forEach() ***/
-        var result = {};
-        angular.forEach(example_products, function(product) {
-          if (product._id == params.id) {
-            return this.product = product;
-          }},
-        result);
-
-        return result.product;
-
-        /*** old-school looping ***/
-        // for(var i=0; i< example_products.length; i++) {
-        //   if(example_products[i]._id == product.id)
-        //     return example_products[i];
-        // }
-        // return null;
-      },
-
-      delete: function(params) {
-        angular.forEach(example_products, function(product, index) {
-          if (product._id == params._id) {
-            console.log(product, index);
-            example_products.splice(index, 1);
-            return;
-          }
-        });
-      },
-
-      create: function(product) {
-        product.id = ++last_id;
-        example_products.push(product);
-      },
-
-      update: function(product) {
-        var item = this.get(product);
-        if (!item) return false;
-
-        item.title = product.title;
-        item.price = product.price;
-        item.quantity = product.quantity;
-        item.description = product.description;
-        return true;
-      }
-    };
+  .factory('Products', function ($resource) {
+    return $resource('/api/products/:id', null, {
+      'update': { method: 'PUT'}
+    });
   });
--- server/config/seed.js
+++ server/config/seed.js
@@ -6,6 +6,7 @@
 'use strict';
 
 var User = require('../api/user/user.model');
+var Product = require('../api/product/product.model');
 
 User.find({}).removeAsync()
   .then(function() {
@@ -23,5 +24,31 @@ User.find({}).removeAsync()
     })
     .then(function() {
       console.log('finished populating users');
+    });
+  });
+
+Product.find({}).removeAsync()
+  .then(function() {
+    Product.createAsync({
+      title: 'Inspiron 5720',
+      imageUrl: '/assets/images/Dell_Inspiron_5720.jpg',
+      price: 1234.99,
+      stock: 250,
+      description: 'Inspiron 5720 is a good laptop.'
+    }, {
+      title: 'MacBook Pro 2016',
+      imageUrl: '/assets/images/macbook-pro-2016.jpg',
+      price: 1999.99,
+      stock: 100,
+      description: 'MacBook Pro laptops are fancy but expensive.'
+    }, {
+      title: 'HP Stream Cloudbook',
+      imageUrl: '/assets/images/hp-stream-3108-002.jpg',
+      price: 349.99,
+      stock: 50,
+      description: 'Cloudbook and Chromebook laptops are like netbooks.'
+    })
+    .then(function() {
+      console.log('finished populating products');
     });
   });
