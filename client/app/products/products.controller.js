669064d5995593834b287001ca19ef0626ba9bdc
Jeff Hong Xiao <jeff.hong.xiao@gmail.com>
2017-01-17 20:54:30 -0400

    make delete, edit, add work

--- client/app/products/products.controller.js
+++ client/app/products/products.controller.js
@@ -8,23 +8,37 @@ angular.module('meanshopApp')
     $scope.product = Products.get({id: $stateParams.id});
 
     $scope.deleteProduct = function() {
-      Products.delete($scope.product);
-      $state.go('products');
+      Products.delete({id: $scope.product._id},
+        function success() {
+          $state.go('products');
+        },
+        errorHandler($scope));
     };
   })
   .controller('ProductNewCtrl', function($scope, $state, Products) {
     $scope.product = {};
 
     $scope.addProduct = function(product) {
-      Products.create($scope.product);
-      $state.go('products');
+      //Products.create($scope.product);
+	  Products.save($scope.product, function(value) {
+		$state.go('viewProduct', {id: value._id});
+	  },
+	  errorHandler($scope));
     };
   })
   .controller('ProductEditCtrl', function($scope, $state, $stateParams, Products) {
     $scope.product = Products.get({id: $stateParams.id});
     $scope.editProduct = function(product) {
-      Products.update($scope.product);
-      $state.go('products');
+      Products.update({id: $scope.product._id}, $scope.product, function(value) {
+		$state.go('viewProduct', {id: value._id});
+	  },
+	  errorHandler($scope));
     }
   })
   ;
+
+var errorHandler = function ($scope) {
+  return function error(httpResponse) {
+    $scope.errors = httpResponse;
+  };
+};
